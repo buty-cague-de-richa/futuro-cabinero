ac33a0c3fd11d64ec1ef581d06e642ac48f6c3d1
el justo <202400893i@utea.edu.pe>
2024-10-04 08:34:15 -0500

    ParImpar

---
+++ prog_java/ParImpar.java
@@ -0,0 +1,27 @@
+package prog_java;
+
+public class ParImpar {
+
+ public static void main(String[] args) {
+
+
+
+
+
+
+ //*declarar las variables 
+ int numero;
+
+ // entrada
+ System.out.println("ingreso un numero");
+numero = integer.paseInt(system.console().readline());
+
+//proceso
+if (numero % 2 == 0) {
+     //salida
+     System.err.println("el numero ingresado es par");   
+ }else{
+     //salida
+     System.out.println("el numero es impar");
+    
+}
